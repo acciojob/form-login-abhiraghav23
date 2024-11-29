3be77120e791ba35f56de7b1e33bf17cfdc21f82
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-29 14:36:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
 
+	 event.preventDefault();
+     
+  const firstName = document.getElementById('fname').value;
+  const lastName = document.getElementById('lname').value;
+
+
+	alert(firstName + " " + lastName);
+	
 }
+document.getElementById('form1').addEventListener('submit', getFormvalue);
